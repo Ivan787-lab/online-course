b6475e1e9065b6b7ca2089d25418b7826a469a71
Ivan <ivanulanovskij143@gmail.com>
2021-04-24 16:02:00 +0500

    header has been completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,28 @@
 import '../css/style.css'
+import '../css/style.scss'
+
+
+/* let allNumbersInTimer = document.querySelectorAll('.time-block__number')
+let second;
+let minute;
+let hour;
+let day;
+
+allNumbersInTimer.forEach(item => {
+    if (item.id == 'second') {
+        second = item
+    }
+    else if (item.id == 'minute') {
+        minute = item
+    } else if (item.id == 'hour') {
+        hour = item
+    } else if (item.id == 'day') {
+        day = item
+    }
+})
+
+allNumbersInTimer.forEach(item => {
+    allNumbersInTimer = Array.from(allNumbersInTimer)
+    second--
+})
+ */
