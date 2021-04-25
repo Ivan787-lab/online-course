f4eb8adc336519adf5751a3a4ad1907f69c76fd6
Ivan <ivanulanovskij143@gmail.com>
2021-04-25 21:02:45 +0500

    block training programm was completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,5 @@
 import '../css/style.css'
 import '../css/style.scss'
 
-
-/* let allNumbersInTimer = document.querySelectorAll('.time-block__number')
-let second;
-let minute;
-let hour;
-let day;
-
-allNumbersInTimer.forEach(item => {
-    if (item.id == 'second') {
-        second = item
-    }
-    else if (item.id == 'minute') {
-        minute = item
-    } else if (item.id == 'hour') {
-        hour = item
-    } else if (item.id == 'day') {
-        day = item
-    }
-})
-
-allNumbersInTimer.forEach(item => {
-    allNumbersInTimer = Array.from(allNumbersInTimer)
-    second--
-})
- */
+let progressValue = document.querySelector('progress').value
+document.querySelector('#progress__span-earned').innerHTML = progressValue + "₽"
